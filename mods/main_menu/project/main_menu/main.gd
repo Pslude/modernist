02e2f07e47e892dfa7dbf6849c76e56b601abe1b
Dylan Ferris <psient@proton.me>
2025-09-18 01:10:28 -0400

    add multiplayer menu

--- mods/main_menu/project/main_menu/main.gd
+++ mods/main_menu/project/main_menu/main.gd
@@ -3,6 +3,9 @@ extends Control
 func _on_new_game_button_pressed():
 	get_tree().change_scene_to_file("res://game/main.tscn")
 
+func _on_multiplayer_button_pressed():
+	get_tree().change_scene_to_file("res://multiplayer_menu/main.tscn")
+
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://settings_menu/main.tscn")
 
