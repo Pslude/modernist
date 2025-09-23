6461df088b93e51b565c02b9049307607084c06b
Dylan Ferris <psient@proton.me>
2025-09-22 22:45:55 -0400

    clean up game node

--- mods/game/project/game/init.gd
+++ mods/game/project/game/init.gd
@@ -1,4 +1,1 @@
 extends Control
-
-func _ready():
-	get_tree().call_deferred("change_scene_to_file", "res://game/main.tscn")
--- mods/game/project/game/main.gd
+++ mods/game/project/game/main.gd
@@ -1,7 +1,1 @@
 extends Control
-
-func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://settings_menu/main.tscn")
-
-func _on_back_to_main_menu_button_pressed():
-	get_tree().change_scene_to_file("res://main_menu/main.tscn")
